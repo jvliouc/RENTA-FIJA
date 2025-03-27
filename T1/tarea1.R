fcb24fba2e8bd490424fcf392e912021e64cf5ad
jvliouc <julio.orozco@uc.cl>
2025-03-27 19:44:35 -0300

    sin avances significativos

--- T1/tarea1.R
+++ T1/tarea1.R
@@ -1,1 +1,8 @@
 #Codigo tarea
+library(readxl)
+BILL <- read_excel("RENTA-FIJA/T1/DATOS.xlsx", 
+                   sheet = "MARKET BASED BILL")
+NOTE <- read_excel("RENTA-FIJA/T1/DATOS.xlsx", 
+                   sheet = "MARKET BASED NOTE")
+BOND <- read_excel("RENTA-FIJA/T1/DATOS.xlsx", 
+                   sheet = "MARKET BASED BOND")
