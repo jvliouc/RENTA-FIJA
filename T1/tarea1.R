19dc230b704d89311af6f738e7d5d82d742a5405
jvliouc <julio.orozco@uc.cl>
2025-04-04 16:40:59 -0300

    no pude hacer c y d final

--- T1/tarea1.R
+++ T1/tarea1.R
@@ -3,6 +3,7 @@ library(readxl)
 library(dplyr)
 library(lubridate)
 library(ggplot2)
+library(xtable)
 BILL <- read_excel("RENTA-FIJA/T1/DATOS.xlsx", 
                    sheet = "MARKET BASED BILL")
 NOTE <- read_excel("RENTA-FIJA/T1/DATOS.xlsx", 
@@ -40,9 +41,17 @@ boot <- boot %>%
   mutate(PRECIO=(BUY+SELL)/2)
 boot_clean <- boot %>%
   select(-names(NOTE))
+#--------------------------------tabla------------------------------------------
+my_table <- boot_clean %>%
+  mutate(`FECHA VENCIMIENTO` = format(`FECHA VENCIMIENTO`, "%Y-%m-%d"))%>%
+  mutate(`TASA CUPON`=`TASA CUPON`*100)
+latex_table <- xtable(my_table)
+print(latex_table, type = "latex")
 #-------------------------------------------------------------------------------
 #voy a crear la matriz de pago p
+#primero se crea una matriz de ceros
 C <- matrix(0, nrow = nrow(boot_clean), ncol = nrow(boot_clean))
+#luego, dado que se sabe que los datos son l.i, se realiza un ciclo for como sigue:
 for (i in 1:nrow(boot_clean)){
   for (j in 1:i){
     if (j<i){
@@ -53,6 +62,11 @@ for (i in 1:nrow(boot_clean)){
   }
 }
 P0 <- boot_clean$PRECIO
+#---------------------------------matrix a latex--------------------------------
+latex_matriz1 <- xtable(C[,1:8])
+latex_matriz2 <- xtable(C[,9:16])
+print(latex_matriz1)
+print(latex_matriz2)
 #-------------------------------------------------------------------------------
 C_inversa<-solve(C)
 Z0<-C_inversa%*%P0
@@ -101,6 +115,13 @@ regresion <- regresion %>%
   mutate(PRECIO=(BUY+SELL)/2)
 regresion_clean <- regresion %>%
   select(-names(NOTE))
+#--------------------------------tabla------------------------------------------
+my_table <- regresion_clean %>%
+  mutate(`FECHA VENCIMIENTO` = format(`FECHA VENCIMIENTO`, "%Y-%m-%d"))%>%
+  mutate(`TASA CUPON`=`TASA CUPON`*100)
+latex_table <- xtable(my_table)
+print(latex_table, type = "latex")
+#-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 #voy a crear la matriz de pago p
 C_reg <- matrix(0, nrow = nrow(regresion_clean), ncol = as.integer(regresion_clean$PLAZO[30]/0.5))
@@ -114,7 +135,12 @@ for (i in 1:nrow(regresion_clean)){
     }
   }
 }
-
+#---------------------------------matrix a latex--------------------------------
+latex_matriz1 <- xtable(C_reg[1:16,1:8])
+latex_matriz2 <- xtable(C_reg[1:16,9:16])
+print(latex_matriz1)
+print(latex_matriz2)
+#-------------------------------------------------------------------------------
 C_reg_trans<-t(C_reg)
 C2_inv<-solve(C_reg_trans%*%C_reg)
 P0_reg<-regresion_clean$PRECIO
@@ -132,7 +158,7 @@ df_plazo_z_R <- df_plazo_z_R %>%
 ggplot(df_plazo_z_R, aes(x = PLAZO, y = Z)) +
   geom_line() +  
   geom_point(color='purple')+
-  ggtitle("Curva de descuento") +
+  ggtitle("Curva de descuento, regresión") +
   xlab("Plazo[años]") +
   ylab("Factor de Descuento [$]")+
   scale_x_continuous(limits = c(0, 8), breaks = seq(0, 10, by = 0.5)) +
@@ -142,7 +168,7 @@ ggplot(df_plazo_z_R, aes(x = PLAZO, y = Z)) +
 ggplot(df_plazo_z_R, aes(x = PLAZO, y = R)) +
   geom_line() +  
   geom_point(color='blue')+
-  ggtitle("Yield curve") +
+  ggtitle("Yield curve, regresión") +
   xlab("Plazo[años]") +
   ylab("Rendimiento[%]")+
   scale_x_continuous(limits = c(0, 8), breaks = seq(0, 10, by = 0.5)) +
@@ -157,8 +183,47 @@ BILL1<-BILL%>%
   mutate(PLAZO = round(as.numeric(difftime(`MATURITY DATE`, as.Date("2025-02-14"), 
                                          units = "days")), 0))%>%
   mutate(PRECIO= 100*(1-(`END OF DAY`/100)*PLAZO/360))%>% #d estaba en porcentaje, en laformula vista en clases no
-  mutate(yield=100*((100/PRECIO) -1))
+  mutate(yield = 100 * ((100 / PRECIO)^(1 / (PLAZO/360)) - 1)) #anualizada
 
 BILL_clean<-BILL1%>%
   select(-names(BILL))
+
+latex_table <- xtable(BILL_clean[1:20,])
+print(latex_table, type = "latex")
 #------------------------------------------------------------------------------#
+BOND_ns<-BOND%>%
+  mutate(u_dia=round(as.numeric(difftime(`MATURITY DATE`,as.Date("2025-02-14"),units = "days")),0)) %>%
+  mutate(PLAZO=u_dia/360)%>%
+  mutate(i_dev=(RATE/2)*(u_dia/180))%>%
+  mutate(P_LIMPIO=`END OF DAY`)%>%
+  mutate(P_SUCIO=P_LIMPIO+i_dev)
+NS_B<-BOND_ns%>%
+  select(`MATURITY DATE`,PLAZO,P_LIMPIO, P_SUCIO)
+NOTE_ns<-NOTE%>%
+  mutate(u_dia=round(as.numeric(difftime(`MATURITY DATE`,as.Date("2025-02-14"),units = "days")),0)) %>%
+  mutate(PLAZO=u_dia/360)%>%
+  mutate(i_dev=(RATE/2)*(u_dia/180))%>%
+  mutate(P_LIMPIO=`END OF DAY`)%>%
+  mutate(P_SUCIO=P_LIMPIO+i_dev)
+NS_N<-NOTE_ns%>%
+  select(`MATURITY DATE`,PLAZO,P_LIMPIO, P_SUCIO)
+NS<-bind_rows(NS_B,NS_N)%>%
+  arrange(`MATURITY DATE`)%>%
+  mutate(`MATURITY DATE` = format(`MATURITY DATE`, "%Y-%m-%d"))%>%
+  rename(`FECHA VENCIMIENTO` = `MATURITY DATE`)
+  
+#-------------------------------------------------------------------------------
+latex_table <- xtable(NS[1:20,])
+print(latex_table, type = "latex")
+#______________________________________________________________________________#
+#AHORA LA MAS DIFICIL necesito mezclar todo, pero para eso necesito que esten en las 
+#mismas variables
+#para la matriz de pagos me conviene que los plazos esten en dias para despues 
+#hacer la matriz de pago cada 2 semanas
+#necesito las tasas cupon anualizadas
+
+# Suponiendo que tus datos están en un data frame llamado bonos
+# y tienes columnas: PLAZO (en años) y P_SUCIO
+#primero mezclo las dt 
+
+## NO PUDE :C
